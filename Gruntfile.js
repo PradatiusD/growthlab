51de3820ad8dbb89efe09838fcbe2a74bbe7a476
PradatiusD <danielprada2012@gmail.com>
2014-10-06 14:22:24 -0400

    Setup livereload workflow

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+
+    watch: {
+      php: {
+        files: ['index.html'],
+        options: {
+          livereload: 35729
+        }
+      }
+    },
+    php: {
+      test: {
+        options: {
+          keepalive: true,
+          open: true
+        }
+      }
+    }
+  });
+
+  // Default task
+  grunt.registerTask('default', ['watch']);
+
+  // Load up tasks
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-php');
+};
