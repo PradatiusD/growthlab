6921372b23c72fe457afc0ddb7a0fef4e158b424
PradatiusD <danielprada2012@gmail.com>
2014-10-09 09:03:11 -0400

    moved data into controller

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,7 @@ module.exports = function(grunt) {
 
     watch: {
       php: {
-        files: ['index.html','style.css'],
+        files: ['index.html','style.css','script.js'],
         options: {
           livereload: 35729
         }
--- script.js
+++ script.js
@@ -1,5 +1,75 @@
 angular.module('growthLab', [])
 
+.controller('FlippyController', ['$scope', function($scope){
+  
+  $scope.cards = [
+    {
+      "width": 303,
+      "height": 324,
+      "front": "img/chart-left-empty.png",
+      "back": "img/cat-happy.jpg",
+      "class": "chart-left-empty"
+    },
+    {
+      "width": 303,
+      "height": 324,
+      "front": "img/chart-left-empty.png",
+      "back": "img/cat-happy.jpg",
+      "class": "chart-left-empty"
+    },
+    {
+      "width": 361,
+      "height": 324,
+      "front": "img/chart-icon.png",
+      "back": "img/cat-happy.jpg",
+      "class": "chart-icon"
+    },
+    {
+      "width": 400,
+      "height": 367,
+      "front": "img/chevron-up-right-icon.png",
+      "back": "img/cat-happy.jpg",
+      "class": "chevron-up-right"
+    },
+    {
+      "width": 653,
+      "height": 454,
+      "front": "img/welcome-text.png",
+      "back": "img/cat-happy.jpg",
+      "class": "welcome-text"
+    },
+    {
+      "width": 155,
+      "height": 146,
+      "front": "img/small-square-top.png",
+      "back": "img/cat-happy.jpg",
+      "class": "small-square-top"
+    },
+    {
+      "width": 155,
+      "height": 107,
+      "front": "img/small-square-bottom.png",
+      "back": "img/cat-happy.jpg",
+      "class": "small-square-bottom"
+    },
+    {
+      "width": 410,
+      "height": 388,
+      "front": "img/binoculars-icon.png",
+      "back": "img/cat-happy.jpg",
+      "class": "binoculars"
+    },
+    {
+      "width": 400,
+      "height": 301,
+      "front": "img/document-icon.png",
+      "back": "img/cat-happy.jpg",
+      "class": "document"
+    }
+  ];
+
+}])
+
 .directive('cardFlip', function(){
 
   var flip = function () {
